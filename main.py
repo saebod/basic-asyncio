d88f01a3573fbf2e4eecc178e1874f15a484e5fd
Sæbo Davtians <sda@initiate.dk>
2022-06-06 13:40:49 +0200

    Modified so it works with dataframes

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
 import asyncio
-import aiohttp
+from aiohttp import ClientSession, ClientTimeout
 import time
 import json
+import pandas as pd # 
+from tqdm import tqdm
 
+# Loads df
+df = pd.read_csv("df.csv")
+#Split columns
+df[['Type', 'Y', 'X']] =df['CelleID_m100'].str.split('_', expand=True)
+df['X'] = df['X'].apply(lambda x: float(x)*100+50)
+df['Y'] = df['Y'].apply(lambda x: float(x)*100+50)
+url = 'https://api.dataforsyningen.dk/kommuner/reverse?x={}&y={}&srid=25832'
 
-
-url = 'https://api.dataforsyningen.dk/postnumre/{}'
-Postnumre = ['22222000', '1455', '2200', '2300', '2400','1000','7130','7140','2000', '1455', '2200', '2300', '2400','1000', '1455', '2200']
+## Asynico settings
+asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+timeout = ClientTimeout(total=6000)
 
 #Creates a lists of the results
 ListOfResults = []
@@ -15,12 +24,12 @@ ListOfResults = []
 def CreateListOfRequests(session):
     '''Creates a list of tasks from each postnumer'''
     tasks = []
-    for postnummer in Postnumre :
-        tasks.append(asyncio.create_task(session.get(url.format(postnummer), ssl=False)))
+    for idx, row in tqdm(df.iterrows()):
+        tasks.append(asyncio.create_task(session.get(url.format(df.loc[idx,'X'],df.loc[idx,'Y']), ssl=False)))
     return tasks
 
 #Function to handle the response
-def GetCityName(response):
+def GetMunicipality(response):
     '''
     Takes the respons and return the city name
     '''
@@ -29,20 +38,19 @@ def GetCityName(response):
     Name=None
     if 'navn' in data:
         Name=data['navn']
+    else:
+        Name='None'
     return Name
 
-
 start = time.time()
-
 #Collect the response
 async def GetResponse():
-    async with aiohttp.ClientSession(trust_env=True) as session:
+    async with ClientSession(trust_env=True,timeout=timeout) as session:
         tasks = CreateListOfRequests(session)
         responses = await asyncio.gather(*tasks)
-        for response in responses:
-            print(GetCityName(await response.json()))
+        for idx, response in enumerate(responses):
+            df.loc[idx,'Kommune']=GetMunicipality(await response.json())
 asyncio.run(GetResponse())
-
 end = time.time()
 total_time = end - start
-print("It took {} seconds to make {} API calls".format(total_time, len(Postnumre)))
+print("It took {} seconds to make {} API calls".format(total_time, len(df)))
